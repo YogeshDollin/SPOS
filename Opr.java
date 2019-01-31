ef16747d483b8fc1779d06bc2ff139a6ca13d7b4
Yogi-official <35768778+Yogi-official@users.noreply.github.com>
2019-01-31 06:37:39 +0530

    optimal page replacement
    implemented completely.testing remains.

--- Opr.java
+++ Opr.java
@@ -46,23 +46,55 @@ public class Opr{
 
     void process(){
         
-        boolean flag = true;
+        boolean flag2 = true ,flag1 = true;
+        int j=0;
 
         for (int i = 0 ; i < 10 ; i++){
+
+            flag2 = true;
+            flag1 = true;
 
             noLongerUsed[0] = getIndex(i,pageDisplay[0][i]);
             noLongerUsed[1] = getIndex(i,pageDisplay[1][i]);
             noLongerUsed[2] = getIndex(i,pageDisplay[2][i]);
 
-            for(int j = 0 ; j < 3 ; j++){
-                if (noLongerUsed[j] == -1)
-                    pageDisplay[j][i] = page[i];
-                    flag = false;
-                    break;
-            }
-            if (flag){
+            System.out.println(noLongerUsed[0]+" "+noLongerUsed[1]+" "+noLongerUsed[2]);
 
-                pageDisplay[getMax()][i] = page[i];
+            if (j < 3){
+                for(j = 0 ; j < 3 ; j++){
+                if (pageDisplay[j][i] == -1){
+                    pageDisplay[j][i] = page[i];
+                    break;
+                 }
+                }
+            }
+
+                for(int k = 0 ; k < 3 ; k++){
+                if (pageDisplay[k][i] == page[i]){
+                    flag1 = false;
+                    break;
+                 }
+                }
+           
+                if(flag1){
+                    for(int k = 0 ; k < 3 ; k++){
+                        if (noLongerUsed[k] == -1){
+                            pageDisplay[k][i] = page[i];
+                        flag2 = false;
+                         break;
+                        }
+                     }
+                     if (flag2){
+                         pageDisplay[getMax()][i] = page[i];
+                    }
+                }
+            
+            
+
+            if((i+1) < 10){
+                pageDisplay[0][i+1] = pageDisplay[0][i];
+                pageDisplay[1][i+1] = pageDisplay[1][i];
+                pageDisplay[2][i+1] = pageDisplay[2][i];
             }
 
         }
@@ -72,6 +104,8 @@ public class Opr{
 
         for (int i = 0 ; i < 10 ; i++)
             System.out.print(page[i]+" ");
+
+    System.out.println();
 
         for( int i = 0 ; i < 3 ; i++){
             for(int j = 0 ; j < 10 ; j++){
